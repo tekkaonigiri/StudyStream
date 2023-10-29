d0cf5be6625155d75273ecaffa6b9208a8024ddf
Nicole <nicolefongjw@gmail.com>
2023-10-29 11:54:01 -0700

    Initial Commit

---
+++ StudyStream/StudyStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyStreamApp.swift
+//  StudyStream
+//
+//  Created by Nicole Fong on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
