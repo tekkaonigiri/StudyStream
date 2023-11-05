69dc1849ae0ac9d2d5e2c621a6790a48a3325ace
Nicole <nicolefongjw@gmail.com>
2023-11-05 13:06:53 -0800

    main page + starting sheets

--- StudyStream/ContentView.swift
+++ StudyStream/ContentView.swift
@@ -1,19 +1,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSetTimeSheet = false
+    @State private var showCountdownSheet = false
+
     var body: some View {
         ZStack {
-            Color(.black)
+            Image("oceanbg")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+
             VStack {
-                Image("purple wallpaper")
-                    .resizable()
-                    .cornerRadius(20)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                Text("Hello")
-                    .font(.title)
+                Text("StudyStream")
+                    .font(
+                        .custom(
+                        "JustMeAgainDownHere",
+                        size: 65))
                     .foregroundColor(.white)
-                    .bold()
+                    .padding(.bottom)
+
+// set timer button
+                Button {
+                    showSetTimeSheet.toggle()
+
+                } label: {
+                    Text("Set timer")
+                        .foregroundColor(.blue)
+                        .font(
+                            .custom(
+                            "JustMeAgainDownHere",
+                            size: 40))
+                }.background(Color.white)
+                    .cornerRadius(30)
+                    .controlSize(.mini)
+                    .buttonStyle(.bordered)
+
+                    .sheet(isPresented: $showSetTimeSheet) {
+//back button
+                        Button {
+                        } label: {
+                            Text("Back")
+                                .foregroundColor(.blue)
+                                .font(
+                                    .custom(
+                                    "JustMeAgainDownHere",
+                                    size: 40))
+                        }.background(Color.white)
+                            .cornerRadius(30)
+                            .controlSize(.mini)
+                            .buttonStyle(.bordered)
+
+//start button
+                        Button {
+                        } label: {
+                            Text("Start")
+                                .foregroundColor(.blue)
+                                .font(
+                                    .custom(
+                                    "JustMeAgainDownHere",
+                                    size: 40))
+                        }.background(Color.white)
+                            .cornerRadius(30)
+                            .controlSize(.mini)
+                            .buttonStyle(.bordered)
+                    }
                 
             }
         }
