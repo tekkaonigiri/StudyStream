c63974a97c592fc1eaa61761b1c5a155e79a3f98
Nicole <nicolefongjw@gmail.com>
2023-11-07 21:27:15 -0800

    +2 sheets
    added 2nd and 3rd sheets - not detailed yet
    helper.swift for initializing Timer

--- StudyStream/ContentView.swift
+++ StudyStream/ContentView.swift
@@ -3,73 +3,111 @@ import SwiftUI
 struct ContentView: View {
     @State private var showSetTimeSheet = false
     @State private var showCountdownSheet = false
-
+    @State var timer: Timer = .initial
+    
     var body: some View {
+//MAIN//
         ZStack {
             Image("oceanbg")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
-
-            VStack {
-                Text("StudyStream")
-                    .font(
-                        .custom(
-                        "JustMeAgainDownHere",
-                        size: 65))
-                    .foregroundColor(.white)
-                    .padding(.bottom)
-
-// set timer button
-                Button {
-                    showSetTimeSheet.toggle()
-
-                } label: {
-                    Text("Set timer")
-                        .foregroundColor(.blue)
-                        .font(
-                            .custom(
-                            "JustMeAgainDownHere",
-                            size: 40))
-                }.background(Color.white)
-                    .cornerRadius(30)
-                    .controlSize(.mini)
-                    .buttonStyle(.bordered)
-
-                    .sheet(isPresented: $showSetTimeSheet) {
-//back button
-                        Button {
-                        } label: {
-                            Text("Back")
-                                .foregroundColor(.blue)
-                                .font(
-                                    .custom(
-                                    "JustMeAgainDownHere",
-                                    size: 40))
-                        }.background(Color.white)
-                            .cornerRadius(30)
-                            .controlSize(.mini)
-                            .buttonStyle(.bordered)
-
-//start button
-                        Button {
-                        } label: {
-                            Text("Start")
-                                .foregroundColor(.blue)
-                                .font(
-                                    .custom(
-                                    "JustMeAgainDownHere",
-                                    size: 40))
-                        }.background(Color.white)
-                            .cornerRadius(30)
-                            .controlSize(.mini)
-                            .buttonStyle(.bordered)
-                    }
+            
+            
+        VStack {
+            Text("StudyStream")
+                .font(.custom("JustMeAgainDownHere",
+                              size: 65))
+                .foregroundColor(.white)
+                .padding(.bottom)
                 
+// set timer button
+            Button {
+                showSetTimeSheet.toggle()
+            } label: {
+                Text("Set timer")
+                    .foregroundColor(.blue)
+                    .font(.custom("JustMeAgainDownHere",
+                                  size: 40))
+            }.background(Color.white)
+                .cornerRadius(30)
+                .controlSize(.mini)
+                .buttonStyle(.bordered)
+                
+                
+//SECOND PAGE//
+                .sheet(isPresented: $showSetTimeSheet) {
+            ZStack {
+                Image("right-oceanbg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                
+            VStack {
+//back button
+            Button {
+                showSetTimeSheet.toggle()
+            } label: {
+                Text("Back")
+                    .foregroundColor(.blue)
+                    .font(.custom("JustMeAgainDownHere",
+                                  size: 40))
+            }.background(Color.white)
+                .cornerRadius(30)
+                .controlSize(.mini)
+                .buttonStyle(.bordered)
+                            
+//timer
+            VStack {
+                Text("60")
+                    .font(.custom("JustMeAgainDownHere",
+                                  size: 40))
+                    .padding(.top, 80)
             }
-        }
+                            
+//start button
+            Button {
+                showCountdownSheet.toggle()
+                showSetTimeSheet.toggle()
+            } label: {
+                Text("Start")
+                    .foregroundColor(.blue)
+                    .font(.custom("JustMeAgainDownHere",
+                                  size: 40))
+            }.background(Color.white)
+                .cornerRadius(30)
+                .controlSize(.mini)
+                .buttonStyle(.bordered)
+                            
+//countdown
+                .sheet(isPresented: $showCountdownSheet) {
+            ZStack {
+                Image("left-oceanbg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+
+//play button
+            VStack {
+                    Image (systemName: timer == .running ? "pause" : "resume")
+                        .resizable()
+                        .aspectRatio (contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.white)
+                    }//countdown vstack
+            }//countdown zstack
+
+            }//3rd page sheet - countdown
+                        }//vstack
+                            }//zstack
+                        }//2nd page sheet - timer
+                        
+                        
+    }//main vstack
+    }//main zstack
+    }//var body some view
+    }//struct content view
+    
+    #Preview {
+        ContentView()
     }
-}
-#Preview {
-    ContentView()
-}
---
+++ StudyStream/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  StudyStream
+//
+//  Created by Nicole Fong on 11/7/23.
+//
+
+// initializes Timer function
+import Foundation
+
+enum Timer {
+case running
+case paused
+case initial
+}
